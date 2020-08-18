3516f753ebaf867e726719dc926c15903a0f220a
chaipa <chaitanya.patel@flemingcollege.ca>
2020-08-17 20:40:24 -0400

    useState by different initial value

--- src/index.js
+++ src/index.js
@@ -1,12 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const OneTimeButton = ({ onClick }) => {
+  const [click, setClick] = useState(false)
+  const [items, setItems] = useState([])
+  const counts = {
+    countA: 0,
+    countB: 0
+  }
+  const [count, setCount] = useState(counts)
+
+
+  const incA = () => {
+    setCount(count => ({
+     ...count,
+      countA: count.countA + 1
+    }) )
+  }
+  
+  const incB = () => {
+    setCount(count => ({
+      ...count,
+      countB: count.countB + 1
+    }) )
+  }
+
+  
+
+  const addItem = () => {
+    setItems([
+      ...items,
+      {
+        id: items.length,
+        value: Math.random() * 100 + 10
+      }
+    ])
+  }
+
+
+  const handleClick = () => {
+    // Ok, no more clicking.
+    onClick();
+    setClick(true);
+  };
+
+
+  return (
+    <div>
+      <button onClick={handleClick}
+        disabled={click}>
+        click me once
+      </button>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>{item.value}</li>
+        ))}
+
+      </ul>
+      <button onClick={addItem}>Add a number</button>
+      <button name="incrementA" onClick={incA}>incA</button>
+      <button name="incrementA" onClick={incB}>incB</button>
+        <h1>A:{count.countA}</h1>
+        <h1>B:{count.countB}</h1>
+
+    </div>
+  )
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <OneTimeButton onClick={() => alert("hii")} />
   </React.StrictMode>,
   document.getElementById('root')
 );
